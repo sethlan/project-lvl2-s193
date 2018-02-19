bce1507a499e683b8a92ea640f89720f34e8803d
sethlan <sethlans@protonmail.com>
2018-02-19 13:39:12 +0800

    add option

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,7 +2,8 @@
 import program from 'commander';
 
 program
-  .version('0.0.4')
+  .version('0.0.6')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
+  .option('-f, --format [type]', 'Output format')
   .parse(process.argv);
