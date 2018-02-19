c65cc29ffc8b220262a27d1829b5f8a6c7507d79
sethlan <sethlans@protonmail.com>
2018-02-19 13:33:21 +0800

    fix env

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,6 +2,7 @@
 import program from 'commander';
 
 program
-  .version('0.0.1')
+  .version('0.0.4')
   .description('Compares two configuration files and shows a difference.')
-  .arguments('<firstConfig> <secondConfig>');
+  .arguments('<firstConfig> <secondConfig>')
+  .parse(process.argv);
