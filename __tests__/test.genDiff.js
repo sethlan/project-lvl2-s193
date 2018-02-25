141e16a8b74ad1f343d949c39205aa4216160938
sethlan <sethlans@protonmail.com>
2018-02-25 11:17:23 +0800

    try rewrite index.js, add test

--- __tests__/test.genDiff.js
+++ __tests__/test.genDiff.js
@@ -9,10 +9,16 @@ const pathIni1 = '__tests__/__fixtures__/before.ini';
 const pathIni2 = '__tests__/__fixtures__/after.ini';
 const pathJsonComp1 = '__tests__/__fixtures__/beforeComp.json';
 const pathJsonComp2 = '__tests__/__fixtures__/afterComp.json';
+const pathYamlComp1 = '__tests__/__fixtures__/beforeComp.yml';
+const pathYamlComp2 = '__tests__/__fixtures__/afterComp.yml';
+const pathIniComp1 = '__tests__/__fixtures__/beforeComp.ini';
+const pathIniComp2 = '__tests__/__fixtures__/afterComp.ini';
 const resultJson = fs.readFileSync('__tests__/__fixtures__/result', 'utf-8');
 const resultYml = fs.readFileSync('__tests__/__fixtures__/result2', 'utf-8');
 const resultIni = fs.readFileSync('__tests__/__fixtures__/result3', 'utf-8');
 const resultJsonComp = fs.readFileSync('__tests__/__fixtures__/result4', 'utf-8');
+const resultYmlComp = fs.readFileSync('__tests__/__fixtures__/result5', 'utf-8');
+const resultIniComp = fs.readFileSync('__tests__/__fixtures__/result6', 'utf-8');
 test('gendiff for JSON', () => {
   expect(`${gendiff(pathJson1, pathJson2)}\n`)
     .toBe(resultJson);
@@ -28,4 +34,12 @@ test('gendiff for INI', () => {
 test('gendiff for recursive JSON', () => {
   expect(`${gendiff(pathJsonComp1, pathJsonComp2)}\n`)
     .toBe(resultJsonComp);
+});
+test('gendiff for recursive YAML', () => {
+  expect(`${gendiff(pathYamlComp1, pathYamlComp2)}\n`)
+    .toBe(resultYmlComp);
+});
+test('gendiff for recursive INI', () => {
+  expect(`${gendiff(pathIniComp1, pathIniComp2)}\n`)
+    .toBe(resultIniComp);
 });
--- src/index.js
+++ src/index.js
@@ -11,23 +11,78 @@ const gendiff = (path1, path2) => {
     '.ini': ini.parse,
   };
   const ext = path.extname(path1);
-  const file1 = fs.readFileSync(path1, 'utf-8');
-  const file2 = fs.readFileSync(path2, 'utf-8');
-  const obj1 = adapter[ext](file1);
-  const obj2 = adapter[ext](file2);
-  const keys = _.union(_.keys(obj1), _.keys(obj2));
-  const result = keys.map((e) => {
-    if (obj1[e] === obj2[e]) {
-      return `  ${e}: ${obj1[e]}`;
-    } else if (obj1[e] && obj2[e]) {
-      return [`- ${e}: ${obj1[e]}`, `+ ${e}: ${obj2[e]}`];
-    } else if (obj1[e]) {
-      return `- ${e}: ${obj1[e]}`;
-    } else if (obj2[e]) {
-      return `+ ${e}: ${obj2[e]}`;
-    }
-    return { e };
-  });
-  return `{\n${_.flatten(result).join('\n')}\n}`;
+  const fileContent1 = fs.readFileSync(path1, 'utf-8');
+  const fileContent2 = fs.readFileSync(path2, 'utf-8');
+  const obj1 = adapter[ext](fileContent1);
+  const obj2 = adapter[ext](fileContent2);
+  // console.log(JSON.stringify(obj1), JSON.stringify(obj2));
+  /* {
+    "plugins":["jest"],
+    "env":{"node":true},
+    "parser":"babel-eslint",
+    "extends":["airbnb-base",
+    "plugin:jest/recommended"],
+    "rules":{"no-console":0}} {"plugins":["jest","flowtype"],
+    "env":{"jest":true},
+    "parser":"babel-eslint",
+    "extends":["airbnb-base","plugin:jest/recommended","plugin:flowtype/recommended"],
+    "rules":{"no-console":0,"react/jsx-no-bind":0}
+  } */
+  const makeDiffAst = (before, after) => {
+    const keys = _.union(_.keys(before), _.keys(after));
+    const ast = keys.map((key) => {
+      if ((before[key] instanceof Array) || (after[key] instanceof Array)) {
+        return {
+          name: key, difference: '', value: '', children: before[key],
+        };
+      }
+      if ((before[key] instanceof Object) && (after[key] instanceof Object)) {
+        return {
+          name: key, difference: '', value: '', children: makeDiffAst(before[key], after[key]),
+        };
+      } else if (before[key] instanceof Object) {
+        return {
+          name: key, difference: '', value: '', children: before[key],
+        };
+      } else if (after[key] instanceof Object) {
+        return {
+          name: key, difference: '', value: '', children: after[key],
+        };
+      }
+      if (before[key] === after[key]) {
+        return {
+          name: key, difference: '', value: before[key], children: '',
+        };
+      } else if (before[key] && after[key]) {
+        return [{
+          name: key, difference: '-', value: before[key], children: '',
+        }, {
+          name: key, difference: '+', value: after[key], children: '',
+        },
+        ];
+      } else if (before[key]) {
+        return {
+          name: key, difference: '-', value: before[key], children: '',
+        };
+      }
+      return {
+        name: key, difference: '+', value: after[key], children: '',
+      };
+    });
+    return ast;
+  };
+  const ast = makeDiffAst(obj1, obj2);
+  console.log(JSON.stringify(ast));
+  const render = (astree) => {
+    if (!astree) { return ''; }
+    const result = astree.map((e) => {
+      if (e.children !== '') {
+        return `  ${e.name}: ${render(e.children)}`;
+      }
+      return `${e.difference} ${e.name}: ${e.value}`;
+    });
+    return `{\n${result.join('\n')}}`;
+  };
+  return render(ast);
 };
 export default gendiff;
