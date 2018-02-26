26000f809ed3bf446b96b93f29e0a0a9cfdbe33a
sethlan <sethlans@protonmail.com>
2018-02-26 16:53:07 +0800

    fix test, add new format

--- __tests__/test.genDiff.js
+++ __tests__/test.genDiff.js
@@ -1,45 +1,31 @@
 import fs from 'fs';
 import gendiff from '../src';
 
-const pathJson1 = '__tests__/__fixtures__/before.json';
-const pathJson2 = '__tests__/__fixtures__/after.json';
-const pathYaml1 = '__tests__/__fixtures__/before.yml';
-const pathYaml2 = '__tests__/__fixtures__/after.yml';
-const pathIni1 = '__tests__/__fixtures__/before.ini';
-const pathIni2 = '__tests__/__fixtures__/after.ini';
-const pathJsonComp1 = '__tests__/__fixtures__/beforeComp.json';
-const pathJsonComp2 = '__tests__/__fixtures__/afterComp.json';
-const pathYamlComp1 = '__tests__/__fixtures__/beforeComp.yml';
-const pathYamlComp2 = '__tests__/__fixtures__/afterComp.yml';
-const pathIniComp1 = '__tests__/__fixtures__/beforeComp.ini';
-const pathIniComp2 = '__tests__/__fixtures__/afterComp.ini';
-const resultJson = fs.readFileSync('__tests__/__fixtures__/result', 'utf-8');
-const resultYml = fs.readFileSync('__tests__/__fixtures__/result2', 'utf-8');
-const resultIni = fs.readFileSync('__tests__/__fixtures__/result3', 'utf-8');
-const resultJsonComp = fs.readFileSync('__tests__/__fixtures__/result4', 'utf-8');
-const resultYmlComp = fs.readFileSync('__tests__/__fixtures__/result5', 'utf-8');
-const resultIniComp = fs.readFileSync('__tests__/__fixtures__/result6', 'utf-8');
 test('gendiff for JSON', () => {
-  expect(`${gendiff(pathJson1, pathJson2)}\n`)
-    .toBe(resultJson);
+  expect(`${gendiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json')}\n`)
+    .toBe(fs.readFileSync('__tests__/__fixtures__/result', 'utf-8'));
 });
 test('gendiff for YAML', () => {
-  expect(`${gendiff(pathYaml1, pathYaml2)}\n`)
-    .toBe(resultYml);
+  expect(`${gendiff('__tests__/__fixtures__/before.yml', '__tests__/__fixtures__/after.yml')}\n`)
+    .toBe(fs.readFileSync('__tests__/__fixtures__/result2', 'utf-8'));
 });
 test('gendiff for INI', () => {
-  expect(`${gendiff(pathIni1, pathIni2)}\n`)
-    .toBe(resultIni);
+  expect(`${gendiff('__tests__/__fixtures__/before.ini', '__tests__/__fixtures__/after.ini')}\n`)
+    .toBe(fs.readFileSync('__tests__/__fixtures__/result3', 'utf-8'));
 });
 test('gendiff for recursive JSON', () => {
-  expect(`${gendiff(pathJsonComp1, pathJsonComp2)}\n`)
-    .toBe(resultJsonComp);
+  expect(`${gendiff('__tests__/__fixtures__/beforeComp.json', '__tests__/__fixtures__/afterComp.json')}\n`)
+    .toBe(fs.readFileSync('__tests__/__fixtures__/result4', 'utf-8'));
 });
 test('gendiff for recursive YAML', () => {
-  expect(`${gendiff(pathYamlComp1, pathYamlComp2)}\n`)
-    .toBe(resultYmlComp);
+  expect(`${gendiff('__tests__/__fixtures__/beforeComp.yml', '__tests__/__fixtures__/afterComp.yml')}\n`)
+    .toBe(fs.readFileSync('__tests__/__fixtures__/result5', 'utf-8'));
 });
 test('gendiff for recursive INI', () => {
-  expect(`${gendiff(pathIniComp1, pathIniComp2)}\n`)
-    .toBe(resultIniComp);
+  expect(`${gendiff('__tests__/__fixtures__/beforeComp.ini', '__tests__/__fixtures__/afterComp.ini')}\n`)
+    .toBe(fs.readFileSync('__tests__/__fixtures__/result6', 'utf-8'));
+});
+test('gendiff for recursive JSON new format', () => {
+  expect(`${gendiff('__tests__/__fixtures__/beforeComp.json', '__tests__/__fixtures__/afterComp.json', 'plain')}\n`)
+    .toBe(fs.readFileSync('__tests__/__fixtures__/result7', 'utf-8'));
 });
--- src/index.js
+++ src/index.js
@@ -61,29 +61,32 @@ const gendiff = (path1, path2, format = 'default') => {
   const diffAst = getAst(obj1, obj2);
   const defaultRender = (ast, level = 1) => {
     const result = ast.map((element) => {
+      const complex = value => (value instanceof Object ? JSON.stringify(value) : value);
       switch (element.type) {
         case 'nested': return `${'\t'.repeat(level)}  ${element.name}: ${defaultRender(element.children, level + 1)}`;
-        case 'not changed': return `${'\t'.repeat(level)}  ${element.name}: ${element.value}`;
-        case 'changed': return `${'\t'.repeat(level)}- ${element.name}: ${element.value.old}\n\t+ ${element.name}: ${element.value.new}`;
-        case 'deleted': return `${'\t'.repeat(level)}- ${element.name}: ${element.value}`;
-        case 'inserted': return `${'\t'.repeat(level)}+ ${element.name}: ${element.value}`;
+        case 'not changed': return `${'\t'.repeat(level)}  ${element.name}: ${complex(element.value)}`;
+        case 'changed': return `${'\t'.repeat(level)}- ${element.name}: ${complex(element.value.old)}\n${'\t'.repeat(level)}+ ${element.name}: ${complex(element.value.new)}`;
+        case 'deleted': return `${'\t'.repeat(level)}- ${element.name}: ${complex(element.value)}`;
+        case 'inserted': return `${'\t'.repeat(level)}+ ${element.name}: ${complex(element.value)}`;
         default: return '';
       }
     });
     return `{\n${result.join('\n')}\n${'\t'.repeat(level - 1)}}`;
   };
-  const plainRender = (ast, level = 1) => {
+  const plainRender = (ast, level = '') => {
     const result = ast.map((element) => {
+      const complex = value => (value instanceof Object ? 'complex value' : value);
+      const name = `${level === '' ? '' : `${level}.`}${element.name}`;
       switch (element.type) {
-        case 'nested': return `${'\t'.repeat(level)}  ${element.name}: ${defaultRender(element.children, level + 1)}`;
-        case 'not changed': return `${'\t'.repeat(level)}  ${element.name}: ${element.value}`;
-        case 'changed': return `${'\t'.repeat(level)}- ${element.name}: ${element.value.old}\n\t+ ${element.name}: ${element.value.new}`;
-        case 'deleted': return `${'\t'.repeat(level)}- ${element.name}: ${element.value}`;
-        case 'inserted': return `${'\t'.repeat(level)}+ ${element.name}: ${element.value}`;
+        case 'nested': return `${plainRender(element.children, name)}`;
+        case 'not changed': return '';
+        case 'changed': return `Property ${name} was updated. From ${complex(element.value.old)} to ${complex(element.value.new)}`;
+        case 'deleted': return `Property ${name} was removed`;
+        case 'inserted': return `Property ${name} was added with value: ${complex(element.value)}`;
         default: return '';
       }
     });
-    return `{\n${result.join('\n')}\n${'\t'.repeat(level - 1)}}`;
+    return result.join('\n');
   };
   const render = format === 'default' ? defaultRender : plainRender;
   return render(diffAst);
