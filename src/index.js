5f26e154be6d7af0dbdf41c26d7ffe6e0aacef39
sethlan <sethlans@protonmail.com>
2018-02-26 15:29:18 +0800

    delete tab, add parsers, switch

--- src/index.js
+++ src/index.js
@@ -4,24 +4,17 @@ import path from 'path';
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-const tab = (times) => {
-  let result = '';
-  for (let i = 0; i < times; i += 1) {
-    result += '\t';
-  }
-  return result;
+const parsers = {
+  '.json': JSON.parse,
+  '.yml': yaml.safeLoad,
+  '.ini': ini.parse,
 };
-const gendiff = (path1, path2) => {
-  const adapter = {
-    '.json': JSON.parse,
-    '.yml': yaml.safeLoad,
-    '.ini': ini.parse,
-  };
+const gendiff = (path1, path2, format = 'default') => {
   const ext = path.extname(path1);
   const fileContent1 = fs.readFileSync(path1, 'utf-8');
   const fileContent2 = fs.readFileSync(path2, 'utf-8');
-  const obj1 = adapter[ext](fileContent1);
-  const obj2 = adapter[ext](fileContent2);
+  const obj1 = parsers[ext](fileContent1);
+  const obj2 = parsers[ext](fileContent2);
   const keyTypes = [{
     type: 'nested',
     check: (first, second, key) => (first[key] instanceof Object && second[key] instanceof Object)
@@ -58,124 +51,28 @@ const gendiff = (path1, path2) => {
         keyTypes,
         item => item.check(firstConfig, secondConfig, key),
       );
-      const value = process(firstConfig[key], secondConfig[key], getAst);
-      return { name: key, type, value };
+      const value = type !== 'nested' ? process(firstConfig[key], secondConfig[key], getAst) : '';
+      const children = type === 'nested' ? process(firstConfig[key], secondConfig[key], getAst) : '';
+      return {
+        name: key, type, value, children,
+      };
     });
   };
   const diffAst = getAst(obj1, obj2);
-  // console.log(JSON.stringify(diffAst));
-  // const renderTypes = [{
-  //   type: 'nested',
-  //   toString: (ast, fun) => fun(ast),
-  // },
-  // {
-  //   type: 'not changed',
-  //   toString: ast => (ast),
-  // },
-  // {
-  //   type: 'changed',
-  //   check: (first, second, key) => (_.has(first, key) && _.has(second, key)
-  //     && (first[key] !== second[key])),
-  //   process: (first, second) => ({ old: first, new: second }),
-  // },
-  // {
-  //   type: 'deleted',
-  //   check: (first, second, key) => (_.has(first, key) && !_.has(second, key)),
-  //   process: first => _.identity(first),
-  // },
-  // {
-  //   type: 'inserted',
-  //   check: (first, second, key) => (!_.has(first, key) && _.has(second, key)),
-  //   process: (first, second) => _.identity(second),
-  // },
-  // ];
-  const render = (ast, level = 1) => {
+  const defaultRender = (ast, level = 1) => {
     const result = ast.map((element) => {
-      if (element.type === 'nested') { return `${tab(level)}  ${element.name}: ${render(element.value, level + 1)}`; }
-      if (element.type === 'not changed') { return `${tab(level)}  ${element.name}: ${element.value}`; }
-      if (element.type === 'changed') { return `${tab(level)}- ${element.name}: ${element.value.old}\n\t+ ${element.name}: ${element.value.new}`; }
-      if (element.type === 'deleted') { return `${tab(level)}- ${element.name}: ${element.value}`; }
-      if (element.type === 'inserted') { return `${tab(level)}+ ${element.name}: ${element.value}`; }
-      return '';
+      switch (element.type) {
+        case 'nested': return `${'\t'.repeat(level)}  ${element.name}: ${defaultRender(element.children, level + 1)}`;
+        case 'not changed': return `${'\t'.repeat(level)}  ${element.name}: ${element.value}`;
+        case 'changed': return `${'\t'.repeat(level)}- ${element.name}: ${element.value.old}\n\t+ ${element.name}: ${element.value.new}`;
+        case 'deleted': return `${'\t'.repeat(level)}- ${element.name}: ${element.value}`;
+        case 'inserted': return `${'\t'.repeat(level)}+ ${element.name}: ${element.value}`;
+        default: return '';
+      }
     });
-    return `{\n${result.join('\n')}\n${tab(level - 1)}}`;
+    return `{\n${result.join('\n')}\n${'\t'.repeat(level - 1)}}`;
   };
+  const render = format === 'default' ? defaultRender : plainRender;
   return render(diffAst);
-  /*
-  const makeDiffAst = (before, after) => {
-    const keys = _.union(_.keys(before), _.keys(after));
-    const ast = keys.map((key) => {
-      if ((before[key] instanceof Array) && (after[key] instanceof Array)) {
-        const result = [];
-        for (let count = 0; count < Math.max(before[key].length, after[key].length); count += 1) {
-          if (after[key].includes(before[key][count])) {
-            result.push({
-              name: '', difference: '', value: before[key][count], children: '',
-            });
-          } else if (before[key][count]) {
-            result.push({
-              name: '', difference: '-', value: before[key][count], children: '',
-            });
-          } else if (after[key][count]) {
-            result.push({
-              name: '', difference: '+', value: after[key][count], children: '',
-            });
-          }
-        }
-        return {
-          name: key, difference: '', value: '', children: result,
-        };
-      }
-      if ((before[key] instanceof Object) && (after[key] instanceof Object)) {
-        return {
-          name: key, difference: '', value: '', children: makeDiffAst(before[key], after[key]),
-        };
-      } else if (before[key] instanceof Object) {
-        return {
-          name: key, difference: '', value: '', children: before[key],
-        };
-      } else if (after[key] instanceof Object) {
-        return {
-          name: key, difference: '', value: '', children: after[key],
-        };
-      }
-      if (before[key] === after[key]) {
-        return {
-          name: key, difference: '', value: before[key], children: '',
-        };
-      } else if (before[key] && after[key]) {
-        return [{
-          name: key, difference: '-', value: before[key], children: '',
-        }, {
-          name: key, difference: '+', value: after[key], children: '',
-        },
-        ];
-      } else if (before[key]) {
-        return {
-          name: key, difference: '-', value: before[key], children: '',
-        };
-      }
-      return {
-        name: key, difference: '+', value: after[key], children: '',
-      };
-    });
-    return ast;
-  };
-  const ast = makeDiffAst(obj1, obj2);
-  console.log(JSON.stringify(ast));
-  const render = (astree) => {
-    if (astree === undefined) { return ''; }
-    console.log(astree);
-    const result = astree.map((e) => {
-      if (e instanceof Array) { return `\t  ${e.name}: ${render(e)}`; }
-      if (e.children !== '') {
-        return `  ${e.name}: ${render(e.children)}`;
-      }
-      if (e.name === '') { return `\t${e.difference} ${e.value}`; }
-      return `${e.difference} ${e.name}: ${e.value}`;
-    });
-    return `{\n${result.join('\n')}}`;
-  };
-  return render(ast); */
 };
 export default gendiff;
